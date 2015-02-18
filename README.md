3225333536e3943aae774924c72fde135c252dec
hisaichi5518 <hisaichi5518@gmail.com>
2015-02-18 20:36:35 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Acme::Imouto - It's new $module
+
+# SYNOPSIS
+
+    use Acme::Imouto;
+
+# DESCRIPTION
+
+Acme::Imouto is ...
+
+# LICENSE
+
+Copyright (C) hisaichi5518.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+hisaichi5518 <hisaichi5518@gmail.com>
---
+++ lib/Acme/Imouto.pm
@@ -0,0 +1,39 @@
+package Acme::Imouto;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Acme::Imouto - It's new $module
+
+=head1 SYNOPSIS
+
+    use Acme::Imouto;
+
+=head1 DESCRIPTION
+
+Acme::Imouto is ...
+
+=head1 LICENSE
+
+Copyright (C) hisaichi5518.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+hisaichi5518 E<lt>hisaichi5518@gmail.comE<gt>
+
+=cut
+
